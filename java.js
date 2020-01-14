2d50a2dccc2876f54dcbb219fe8c69bdcc049d05
Apostoliuc22 <apostoliuc.alex@gmail.com>
2020-01-14 09:29:46 +0200

    java

---
+++ java.js
@@ -0,0 +1,8 @@
+var mycurrentage = "27";
+        if (mycurrentage >= '25') {
+            document.write ("I am over 25 years old");
+
+        } else {
+            document.write ("I am under 25");
+
+        }
